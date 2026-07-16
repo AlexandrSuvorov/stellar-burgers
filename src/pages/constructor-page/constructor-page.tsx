c032e67f71e5de6b37eb08923c60f36e79bbf8ec
AlexandrSuvorov <alexandrsuvorov2001@gmail.com>
2026-07-16 21:48:10 +0300

    add test burger-constructor on playwright

--- src/pages/constructor-page/constructor-page.tsx
+++ src/pages/constructor-page/constructor-page.tsx
@@ -11,7 +11,9 @@ export const ConstructorPage: FC = () => {
   return (
     <>
       {isIngredientsLoading ? (
-        <Preloader />
+        <div data-testid='loading-ingredients'>
+          <Preloader />
+        </div>
       ) : (
         <main className={styles.containerMain}>
           <h1
