785837f80bfddf8c37ab316bfe8763eb4ff6c988
AlexandrSuvorov <alexandrsuvorov2001@gmail.com>
2026-07-16 18:34:01 +0300

    Stop record mock data ingredients

--- tests/ingredients.spec.ts
+++ tests/ingredients.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('Записываем HAR файл ингредиентов', async ({ page }) => {
   await page.routeFromHAR('./tests/hars/ingredients.har', {
     url: '**/ingredients',
-    update: true
+    update: false
   });
 
   await page.goto('/');
